d94895267d2ffd644e39258274edefacdd22e8c9
AntrikshNarang <itsantriksh@gmail.com>
2023-01-07 16:12:37 +0530

    Implementation Completed! Designing Part is Left

--- index.js
+++ index.js
@@ -1,5 +1,36 @@
-var newDiv = document.createElement("div");
-// var newText = document.createTextNode(prompt("What is your name"));
-// newDiv.appendChild(newText);
-// document.querySelector(".input").insertBefore(newDiv,document.querySelector("input"));
-// document.querySelector(".input").insertAdjacentHTML("afterbegin","<div>Hello</div>");
+var charEntered;
+var taskName="";
+var currentElement;
+document.querySelector(".input input").addEventListener("keydown",addtask);
+
+function addtask(){
+    if(event.keyCode>=65 && event.keyCode<=91){
+    taskName = document.querySelector(".addToDo").value;
+    console.log(taskName);
+}
+if(event.key === "Enter"){
+    if(taskName !== ""){
+        taskName = document.querySelector(".addToDo").value;
+        document.querySelector(".todolist").insertAdjacentHTML("afterbegin","<div>" +taskName+ "</div>");
+        document.querySelector(".todolist div").classList.add("task");
+        document.querySelector(".addToDo").value="";
+        resetEventListeners();
+    }
+    // document.querySelector(".addToDo").blur();
+
+}
+}
+function resetEventListeners(){
+
+for(i=0;i<document.querySelectorAll(".task").length;i++){
+    document.querySelectorAll(".task")[i].removeEventListener("click",taskCompleted);
+}
+for(i=0;i<document.querySelectorAll(".task").length;i++){
+    document.querySelectorAll(".task")[i].addEventListener("click",taskCompleted);
+}
+}
+function taskCompleted(){
+        currentElement=this;
+        currentElement.classList.add("lowerOpacity");
+        setTimeout(function(){currentElement.classList.add("hide");},300);
+}
