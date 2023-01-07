05f1b07e3203e894146f0e16f2611c85943f2221
AntrikshNarang <itsantriksh@gmail.com>
2023-01-07 15:22:33 +0530

    Making a ToDo List Site: Added Initial Files

---
+++ index.js
@@ -0,0 +1,5 @@
+var newDiv = document.createElement("div");
+// var newText = document.createTextNode(prompt("What is your name"));
+// newDiv.appendChild(newText);
+// document.querySelector(".input").insertBefore(newDiv,document.querySelector("input"));
+// document.querySelector(".input").insertAdjacentHTML("afterbegin","<div>Hello</div>");
